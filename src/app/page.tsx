e3f0cae03e735ed91989d90514a1c020df8483ac
Kayk-Rios <manjac688@gmail.com>
2024-10-19 20:34:56 -0300

    feat:AtualizandoInformaçoesPages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,36 @@
+"use client";
+
 import ImagePrimary from "@/components/Imgames_logos/ImagePrimary";
-import { Footer } from "@/components/templates/Footer";
-import NavPrimary from "@/components/templates/NavPrimary"
+import ModalOrcamento from "@/components/modal/ModalOrcamento";
+import Pagina from "@/components/templates/Pagina";
 import Patters from "@/components/templates/Patthers";
+import { useState } from "react";
 
- 
-
- 
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>
-      <NavPrimary/>
-      <div className="h-5 separacao">
+    <Pagina>
+        <div className="h-5 separacao"></div>
+      <div className="flex mt-9 items-center justify-around h-[500px] sobre ">
+        <section className="jus flex-col w-[729px]">
+          <h1>
+          Em nossa plataforma, oferecemos soluções tecnológicas personalizadas que vão desde migrações de servidores on-premise para a nuvem até consultorias em TI e auditorias de segurança. Além disso, incentivamos a implementação de políticas de igualdade de gênero, oferecendo serviços exclusivos para empresas que atingem metas de diversidade. Com nosso suporte, sua empresa pode acessar treinamentos, desenvolvimento de software sob medida e monitoramento de infraestrutura, tudo isso enquanto promove uma cultura mais inclusiva e preparada para o futuro. Transforme sua organização em um exemplo de inovação e igualdade com nossos serviços especializados.
+
+          </h1>
+          <button
+            className="ml-[20%] mt-[5%] button rounded-xl"
+            onClick={() => setShowModal(true)}
+          >
+            Realizar Orçamento
+          </button>
+          {showModal && <ModalOrcamento setShowModal={setShowModal} />}
+        </section>
+        <ImagePrimary />
+        
       </div>
-        <div className="flex mt-9 items-center justify-around h-[500px]">
-          <section className="jus flex-col w-[729px] ">
-            <h1>Em nossa plataforma, oferecemos soluções tecnológicas personalizadas que vão desde migrações de servidores on-premise para a nuvem até consultorias em TI e auditorias de segurança. Além disso, incentivamos a implementação de políticas de igualdade de gênero, oferecendo serviços exclusivos para empresas que atingem metas de diversidade. Com nosso suporte, sua empresa pode acessar treinamentos, desenvolvimento de software sob medida e monitoramento de infraestrutura, tudo isso enquanto promove uma cultura mais inclusiva e preparada para o futuro. Transforme sua organização em um exemplo de inovação e igualdade com nossos serviços especializados.</h1>
-            <button className=" ml-[20%]  mt-[5%] button rounded-xl ">Realizar Orçamento</button>
-          </section>
-            <ImagePrimary/>
-        </div>
-
-      <Patters/>
-    <Footer/>
-
-    </div>
+      <Patters />
+    </Pagina>
   );
 }
