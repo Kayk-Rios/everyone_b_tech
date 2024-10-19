e5b2357163b1b83d1a0ae62e2cceea7778e34b13
Kayk-Rios <manjac688@gmail.com>
2024-10-19 20:35:53 -0300

    feat:footerTemplate

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -2,16 +2,17 @@ import Instagram from "../Imgames_logos/Instagram";
 import Linkedin from "../Imgames_logos/Linkedin";
 
 
+interface FooterProps {
+    cor: string;
+  }
+  
 
 
-interface PropsFooter {
-
-}
-export function Footer(props : PropsFooter){
+export function Footer({ cor }: FooterProps){
     return(
         <>
 
-        <div className="flex justify-around items-center h-[134px]">
+        <div className="flex justify-around items-center h-[134px] text-slate-200 " style={{ backgroundColor: cor }}>
             <span>  © 2024 Green Ice. Todos os direitos reservados.  </span>
             <div className="flex flex-col">
                 <h2>Desenvolvedores</h2>
