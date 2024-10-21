653f18693a97e1da6210db1987de722fbc13ca48
Kayk-Rios <manjac688@gmail.com>
2024-10-21 15:03:22 -0300

    style:imagemRespansiva

--- src/components/Imgames_logos/ImageLogo.tsx
+++ src/components/Imgames_logos/ImageLogo.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Logo from "@/components/assets/logo.png";
+
 export default function ComponentName() {
   return (
     <>
-        <Image src={Logo} width={260} height={75} alt="Picture of the author" />
+      <div className="w-[260px] sm:w-[200px] md:w-[240px] lg:w-[260px] xl:w-[260px] 2xl:w-[260px]">
+        <Image src={Logo} alt="Picture of the author" layout="responsive" width={260} height={75} />
+      </div>
     </>
   );
 }
--- src/components/Imgames_logos/ImagePrimary.tsx
+++ src/components/Imgames_logos/ImagePrimary.tsx
@@ -3,7 +3,9 @@ import ImagePrimary from "@/components/assets/imagemPrimary.png";
 export default function ComponentName() {
   return (
     <>
-        <Image src={ImagePrimary} width={597} height={373} alt="Picture of the author" />
+        <div className="  lg:w-[460px] xl:w-[597px] 2xl:w-[597px] md:block hidden">
+          <Image src={ImagePrimary} width={597} height={373} alt="Picture of the author" />
+        </div>
     </>
   );
 }
