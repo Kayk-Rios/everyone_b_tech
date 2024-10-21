9f83d84fedd589db8c664649b743215915516e81
Kayk-Rios <manjac688@gmail.com>
2024-10-21 15:03:56 -0300

    style:gridapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <Pagina>
         <div className="h-5 separacao"></div>
-      <div className="flex mt-9 items-center justify-around h-[500px] sobre ">
-        <section className="jus flex-col w-[729px]">
-          <h1>
+      <div className="  mt-6 grid grid-cols-1 md:grid-cols-2   items-center justify-around h-[500px] sobre p-[2%] ">
+        <section className="flex-col  ">
+          <h1 className="   " >
           Em nossa plataforma, oferecemos soluções tecnológicas personalizadas que vão desde migrações de servidores on-premise para a nuvem até consultorias em TI e auditorias de segurança. Além disso, incentivamos a implementação de políticas de igualdade de gênero, oferecendo serviços exclusivos para empresas que atingem metas de diversidade. Com nosso suporte, sua empresa pode acessar treinamentos, desenvolvimento de software sob medida e monitoramento de infraestrutura, tudo isso enquanto promove uma cultura mais inclusiva e preparada para o futuro. Transforme sua organização em um exemplo de inovação e igualdade com nossos serviços especializados.
 
           </h1>
