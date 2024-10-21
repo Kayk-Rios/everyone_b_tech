2dbd35cd27ea6b09524550c642e70b933848ed9e
Kayk-Rios <manjac688@gmail.com>
2024-10-21 15:02:03 -0300

    feat:addNavBar

--- src/components/templates/NavPrimary.tsx
+++ src/components/templates/NavPrimary.tsx
@@ -1,33 +1,94 @@
 import ImageLogo from "@/components/Imgames_logos/ImageLogo";
-import Link from "next/link";
 
 
- 
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Sobre", href: "/", current: true },
+  { name: "Serviços", href: "/layout/servicos", current: false },
+  { name: "Login", href: "/layout/login", current: false },
+  { name: "Cadastro", href: "/layout/cadastro", current: false },
+];
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 interface PropsNav {}
 export default function NavPrimary(props: PropsNav) {
   return (
     <>
-      <nav className="nav flex justify-evenly items-center h-[102px]">
-       <a href="/"><ImageLogo/></a>
-        <div className="flex gap-40 navPrimaryAflex">
-       
-          <Link href="/">
-          Sobre
-          </Link>
-        
-          <Link href="/layout/servicos">
-          Serviços
-          </Link>
-          
-          <Link href="/layout/login">
-          Login
-          </Link>
-        
-          <Link href="/layout/cadastro">
-          Cadastro
-          </Link>
-        </div>
+      <nav className="nav  justify-around items-center h-[100px]">
+        <Disclosure as="nav" className=" nav bg-gray-800  h-24 items-center justify-between">
+          <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block h-6 w-6 group-data-[open]:hidden"
+                  />
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="hidden h-6 w-6 group-data-[open]:block"
+                  />
+                </DisclosureButton>
+              </div>
+              <div className="flex  flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                 <ImageLogo/>
+                </div>
+                <div className= " mt-[20px] hidden sm:ml-6 sm:block  ">
+                  <div className="flex space-x-32 menu ml-[50px]">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        aria-current={item.current ? "page" : undefined}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white  "
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "rounded-md px-3 py-2 text-sm font-medium "
+                        )}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Opçoes menu */}
+          <DisclosurePanel className="sm:hidden nav ">
+            <div className="space-y-1 px-2 pb-3 pt-2 nav menu">
+              {navigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  aria-current={item.current ? "page" : undefined}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </Disclosure>
       </nav>
     </>
   );
