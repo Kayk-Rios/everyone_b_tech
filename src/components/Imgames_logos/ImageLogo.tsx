c23e0714d9e7e3de0f3e6dd67549d09f7b9a1753
Kayk-Rios <manjac688@gmail.com>
2024-10-21 15:14:55 -0300

    router:LogoParaHome

--- src/components/Imgames_logos/ImageLogo.tsx
+++ src/components/Imgames_logos/ImageLogo.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Logo from "@/components/assets/logo.png";
+import Link from "next/link";
 
 export default function ComponentName() {
   return (
     <>
       <div className="w-[260px] sm:w-[200px] md:w-[240px] lg:w-[260px] xl:w-[260px] 2xl:w-[260px]">
-        <Image src={Logo} alt="Picture of the author" layout="responsive" width={260} height={75} />
+        <Link href="/">
+          <Image src={Logo} alt="Picture of the author" layout="responsive" width={260} height={75} />
+        </Link>
       </div>
     </>
   );
