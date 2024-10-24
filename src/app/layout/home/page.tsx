572bbd999967ee39d88a3e72dc8c61d9b93844d7
Kayk-Rios <manjac688@gmail.com>
2024-10-24 14:22:31 -0300

    feat:NavHome/PageHome

--- src/app/layout/home/page.tsx
+++ src/app/layout/home/page.tsx
@@ -1,8 +1,16 @@
+import { Footer } from "@/components/templates/Footer";
+import NavUnic from "@/components/templates/NavUnic";
 
 export default function Home(){
     return(
         <>
-        
+        <div>
+            <NavUnic/>
+            <section className="h-[600px] corLogado">
+
+            </section>
+            <Footer cor="#203543" />
+        </div>
         </>
     )
 }
