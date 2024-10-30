dc99d2ad26f6c4c8f64988befe255a4f019423b5
Kayk-Rios <manjac688@gmail.com>
2024-10-30 15:06:20 -0300

    Todos

--- src/app/layout/home/page.tsx
+++ src/app/layout/home/page.tsx
@@ -1,13 +1,22 @@
 import { Footer } from "@/components/templates/Footer";
 import NavUnic from "@/components/templates/NavUnic";
+import Add from "@/components/ui/add";
 
 export default function Home(){
     return(
         <>
         <div>
             <NavUnic/>
-            <section className="h-[600px] corLogado">
+            <section className="h-[600px] corLogado p-9 ">
+                <div className=" flex justify-around">
+                    <h1 className="flex items-center">Tickets <span className="bg-green-400 rounded-full text-lg ml-8 cursor-pointer"><Add/></span></h1>
+                    <article><h2>Metas de Igualdade</h2>
+                      {/* Aqui ficara a barra de status  */}
+                    </article>
+                  
+                </div>
 
+                {/* Aqui ficara a lista de tickets */}
             </section>
             <Footer cor="#203543" />
         </div>
--- src/components/Imgames_logos/ImageLogo.tsx
+++ src/components/Imgames_logos/ImageLogo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Logo from "@/components/assets/logo.png";
 import Link from "next/link";
 
-export default function ComponentName() {
+export default function ComponentLogo() {
   return (
     <>
       <div className="w-[260px] sm:w-[200px] md:w-[240px] lg:w-[260px] xl:w-[260px] 2xl:w-[260px]">
